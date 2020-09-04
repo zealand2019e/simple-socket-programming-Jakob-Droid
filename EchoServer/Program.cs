fac1d2820a793c7786584e0fe4cad4258ad6824a
Jakob-Droid <58071936+Jakob-Droid@users.noreply.github.com>
2020-09-04 10:27:19 +0200

    Done Done

--- EchoServer/Program.cs
+++ EchoServer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 
 namespace EchoServer
 {
@@ -7,6 +11,39 @@ namespace EchoServer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Server server = new Server();
+
+            server.Start();
         }
     }
+    public class Server
+    {
+        public void Start()
+        {
+            TcpListener serverSide = new TcpListener(IPAddress.Loopback, 7777);
+            serverSide.Start();
+            TcpClient socket = serverSide.AcceptTcpClient();
+            using(socket)
+            {
+                Stream ns = socket.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+
+                string line = sr.ReadLine();
+                sw.WriteLine(line);
+                sw.AutoFlush = true;
+
+                Console.WriteLine("Server activated");
+                Console.WriteLine(line);
+            }
+        }
+    }
+
+
+
+
+
+
+
 }
